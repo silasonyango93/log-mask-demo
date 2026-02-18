1dd5b039aaaf1d263721c8d7344c28a03f994809
silasonyango93 <silas.onyango93@gmail.com>
2026-02-18 16:45:43 +0300

    feat(log-masking-demo): introduce interface for service layer

--- books-api-demo/src/main/java/com/kcb/interview/silasonyango/books/controller/BookController.java
+++ books-api-demo/src/main/java/com/kcb/interview/silasonyango/books/controller/BookController.java
@@ -1,7 +1,7 @@
 package com.kcb.interview.silasonyango.books.controller;
 
 import com.kcb.interview.silasonyango.books.dto.BookDto;
-import com.kcb.interview.silasonyango.books.service.BookService;
+import com.kcb.interview.silasonyango.books.service.impl.BookServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookController {
 
-  private final BookService service;
+  private final BookServiceImpl service;
 
   @PostMapping
   public ResponseEntity<BookDto> create(@RequestBody BookDto dto) {
--- books-api-demo/src/main/java/com/kcb/interview/silasonyango/books/service/BookService.java
+++ books-api-demo/src/main/java/com/kcb/interview/silasonyango/books/service/BookService.java
@@ -1,73 +1,17 @@
 package com.kcb.interview.silasonyango.books.service;
 
-import com.kcb.interview.silasonyango.books.entity.BookEntity;
 import com.kcb.interview.silasonyango.books.dto.BookDto;
-import com.kcb.interview.silasonyango.books.mapper.BookMapper;
-import com.kcb.interview.silasonyango.books.repository.BookRepository;
-import com.kcb.interview.silasonyango.books.util.JsonUtil;
-import com.kcb.interview.silasonyango.logmask.starter.core.PIIMaskingService;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class BookService {
+public interface BookService {
 
-    private static final Logger log = LoggerFactory.getLogger(BookService.class);
+  BookDto create(BookDto dto);
 
-    private final BookRepository repository;
-    private final JsonUtil jsonUtil;
-    private final BookMapper bookMapper;
+  List<BookDto> findAll();
 
-    public BookDto create(BookDto dto) {
+  BookDto findById(Long id);
 
-        log.info("Creating book (masked): {}", jsonUtil.toJsonString(dto));
+  BookDto update(Long id, BookDto dto);
 
-        //Save created book to database
-        BookEntity bookRequest = bookMapper.toEntity(dto);
-        BookEntity saved = repository.save(bookRequest);
-
-        BookDto result = bookMapper.toDto(saved);
-        log.info("Created book (masked): {}", jsonUtil.toJsonString(result));
-
-        return result;
-    }
-
-    public List<BookDto> findAll() {
-        List<BookDto> result = repository.findAll().stream().map(bookMapper::toDto).toList();
-        log.info("Listing books (masked): {}", jsonUtil.toJsonString(result));
-        return result;
-    }
-
-    public BookDto findById(Long id) {
-        log.info("Finding book by id: {}", id);
-        BookDto result = repository.findById(id).map(bookMapper::toDto).orElseThrow();
-        log.info("Found book (masked): {}", jsonUtil.toJsonString(result));
-        return result;
-    }
-
-    public BookDto update(Long id, BookDto dto) {
-        log.info("Updating book {} with data (masked): {}", id, jsonUtil.toJsonString(dto));
-
-        BookEntity bookEntity = repository.findById(id).orElseThrow();
-        bookEntity.setTitle(dto.getTitle());
-        bookEntity.setAuthor(dto.getAuthor());
-        bookEntity.setEmail(dto.getEmail());
-        bookEntity.setPhoneNumber(dto.getPhoneNumber());
-        BookDto result = bookMapper.toDto(repository.save(bookEntity));
-
-        log.info("Updated book (masked): {}", jsonUtil.toJsonString(result));
-
-        return result;
-    }
-
-    public void delete(Long id) {
-        log.info("Deleting book with id: {}", id);
-        repository.deleteById(id);
-    }
+  void delete(Long id);
 }
-
