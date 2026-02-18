6301a20e9ce1cf94de37d29ee281a7950474dc23
silasonyango93 <silas.onyango93@gmail.com>
2026-02-18 15:53:13 +0300

    feat(log-masking-demo): enhance controller with rest entity wrappers

---
+++ books-api-demo/src/main/java/com/kcb/interview/silasonyango/books/controller/BookController.java
@@ -0,0 +1,60 @@
+package com.kcb.interview.silasonyango.books.controller;
+
+import com.kcb.interview.silasonyango.books.dto.BookDto;
+import com.kcb.interview.silasonyango.books.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/client-api/v1/book-management/books")
+@RequiredArgsConstructor
+public class BookController {
+
+  private final BookService service;
+
+  @PostMapping
+  public ResponseEntity<BookDto> create(@RequestBody BookDto dto) {
+
+    BookDto created = service.create(dto);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(created);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<BookDto>> all() {
+
+    List<BookDto> books = service.findAll();
+
+    return ResponseEntity.ok(books);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<BookDto> byId(@PathVariable Long id) {
+
+    BookDto book = service.findById(id);
+
+    return ResponseEntity.ok(book);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<BookDto> update(@PathVariable Long id,
+      @RequestBody BookDto dto) {
+
+    BookDto updated = service.update(id, dto);
+
+    return ResponseEntity.ok(updated);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+    service.delete(id);
+
+    return ResponseEntity.noContent().build();
+  }
+}
+
